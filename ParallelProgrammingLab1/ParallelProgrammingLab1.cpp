04271b421f108dc98e9396d24eaad4f428aded62
aboya <boyaroff2014@gmail.com>
2022-10-07 10:59:26 +0400

    Lab1

--- ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
+++ ParallelProgrammingLab1/ParallelProgrammingLab1.cpp
@@ -80,132 +80,172 @@
 // OpenMP
 
  
-//#include <omp.h>
-//#include "stdio.h"
-//#include <iostream>
-//#include <ctime>
-//#define CHUNK 100
-//#define NMAX 10000000
-//
-//double reduction(double* a, double sum, int i) {
-//#pragma omp parallel for shared(a) private(i) reduction(+: sum) 
-//    for (i = 0; i < NMAX; i++) {
-//        sum = sum + a[i];
-//    }
-//    return sum;
-//}
-//
-//double critical(double* a, double sum, int i) {
-//#pragma omp parallel for 
-//    for (i = 0; i < NMAX; i++) {
-//#pragma omp critical
-//        {
-//            sum = sum + a[i];
-//        }
-//    }
-//    return sum;
-//}
-//
-//double atomic(double* a, double sum, int i) {
-//#pragma omp parallel for shared(a) 
-//    for (i = 0; i < NMAX; i++) {
-//#pragma omp atomic
-//        sum = sum + a[i];
-//    }
-//    return sum;
-//}
-//
-//
-//int main(int argc, char* argv[]) {
-//    omp_set_num_threads(1);
-//    int i;
-//    double  sum;
-//    srand(time(0));
-//    double* a = (double*)malloc(sizeof(double) * NMAX);
-//    for (i = 0; i < NMAX; i++)
-//    {
-//        a[i] = 1;
-//    }
-//    double st_time, end_time;
-//    st_time = omp_get_wtime();
-//    sum = 0;
-//
-//    sum = reduction(a, sum, i);
-//    //sum = critical(a, sum, i);
-//    //sum = atomic(a, sum, i);
-//
-//    end_time = omp_get_wtime();
-//    end_time = end_time - st_time;
-//    printf("\nTotal Sum = %10.2f", sum);
-//    printf("\nTIME OF WORK IS %f \n", end_time);
-//    free(a);
-//    return 0;
-//}
+#include <omp.h>
+#include <cstdio>
+#include <iostream>
 
 
+//#define Critical
+//#define Atomic
+#define Reduction
+
+
+#define NMAX 6000000
+#define N 3
+#define Q 18
+
+
+double reduction(const float* a, double sum, int i) {
+#pragma omp parallel  for shared(a)  private(i) reduction(+: sum)
+    for (i = 0; i < NMAX; ++i) { sum = sum + a[i]; }
+    return sum;
+}
+double critical(const float* a, double sum, int i) {
+#pragma omp parallel  for
+    for (i = 0; i < NMAX; ++i) {
+#pragma omp critical
+        sum = sum + a[i];
+    }
+    return sum;
+}
+double atomic(const float* a, double sum, int i) {
+#pragma omp  parallel  for  shared(a)
+    for (i = 0; i < NMAX; ++i) {
+#pragma omp atomic
+        sum += a[i];
+    }
+    return sum;
+}
+void OpenMP(int, char* []);
+void OpenMpSequientel(int, char* []);
+
+extern const int NORMAL_SPREAD = 12;
+
+int main(int argc, char* argv[]) {
+    //OpenMpSequientel(argc, argv);
+    OpenMP(argc, argv);
+    return 0;
+}
+void OpenMpSequientel(int argc, char* argv[]) {
+    double st_time = omp_get_wtime(), end_time;
+    for (int i = 0; i < NORMAL_SPREAD; ++i) {
+        float  sum = 0.0;
+        auto* a = new float[NMAX];
+        std::fill(a, a + NMAX, 1.0);
+        for (int j = 0; j < Q; ++j) {
+            for (int k = 0; k < NMAX; ++k) sum += a[k];
+        }
+        sum /= Q;
+        printf("\nSum = %10.2f", sum);
+    }
+    end_time = omp_get_wtime();
+    end_time = (end_time - st_time) / NORMAL_SPREAD;
+    printf("\nTime = %f \n", end_time);
+}
+void OpenMP(int argc, char* argv[]) {
+    int num = N;
+    omp_set_num_threads(num);
+    printf("\nProcess num = %d", num);
+    int i = 0;
+    float  sum = 0.0;
+    auto* a = new float[NMAX];
+    std::fill(a, a + NMAX, 1);
+    double st_time, end_time;
+    st_time = omp_get_wtime();
+
+#ifdef Critical
+    printf("\nType = Critical");
+    for (int j = 0; j < Q; ++j) { sum = critical(a, sum, i); }
+    sum /= Q;
+#endif
+#ifdef Atomic
+    printf("\nType = Atomic");
+    for (int j = 0; j < Q; ++j) { sum = atomic(a, sum, i); }
+    sum /= Q;
+#endif
+#ifdef Reduction
+    printf("\nType = Reduction");
+    for (int j = 0; j < Q; ++j) { sum = reduction(a, sum, i); }
+    sum /= Q;
+#endif
+
+    printf("\nSum = %10.2f", sum);
+    end_time = omp_get_wtime();
+    end_time = end_time - st_time;
+    printf("\nTime = %f \n", end_time);
+    delete[] a;
+}
 
 
 
 
 // MPI
 
-#include <stdlib.h>
-#include "mpi.h"
-#include <stdio.h>
-#include <math.h>
-
-#define NMAX 1000000
-int main(int argc, char* argv[])
-{
-	double x[NMAX], TotalSum, ProcSum = 0.0;
-	int ProcRank, ProcNum, N = 1000000, i;
-	MPI_Status Status;
-
-	double st_time, end_time;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0)
-	{
-		for (i = 0; i < NMAX; i++)
-			    {
-			        x[i] = 1;
-			    }
-	}
-	st_time = MPI_Wtime();
-
-	int k = N / ProcNum;
-	int i1 = k * ProcRank;
-	int i2 = k * (ProcRank + 1);
-
-	if (ProcRank == ProcNum - 1) i2 = N;
-	for (i = i1; i < i2; i++)
-		ProcSum = ProcSum + x[i];
-
-	if (ProcRank == 0)
-	{
-		TotalSum = ProcSum;
-		for (i = 1; i < ProcNum; i++)
-		{
-			MPI_Recv(&ProcSum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &Status);
-			TotalSum = TotalSum + ProcSum;
-		}
-	}
-	else
-		MPI_Send(&ProcSum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	end_time = MPI_Wtime();
-	end_time = end_time - st_time;
-
-	if (ProcRank == 0)
-	{
-		printf("\nTotal Sum = %10.2f", TotalSum);
-		printf("\nTIME OF WORK IS %f ", end_time);
-	}
-
-	MPI_Finalize();
-	return 0;
-}
+//#include "mpi.h"
+//#include <cstdio>
+//#include <algorithm>
+//#include <iostream>
+//#include <fstream>
+//
+////#define Point_Point
+//#define Collect
+//#define Q 18
+//#define NMAX 6000000
+//
+//
+//int main(int argc, char* argv[])
+//{
+//    double TotalSum, ProcSum = 0.0;
+//    int ProcRank, ProcNum;
+//    float* a = nullptr;
+//    MPI_Status Status;
+//    double st_time, end_time;
+//
+//    MPI_Init(&argc, &argv);
+//    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+//    if (ProcRank == 0) {
+//        printf("\nPorcess number = %d", ProcNum);
+//        a = new float[NMAX];
+//        std::fill(a, a + NMAX, 1.0);
+//    }
+//    st_time = MPI_Wtime();
+//
+//    int k = NMAX / ProcNum;
+//    auto* localArray = new float[k];
+//    MPI_Scatter(a, k, MPI_FLOAT, localArray, k, MPI_FLOAT, 0, MPI_COMM_WORLD);
+//    if (ProcRank == 0) delete[] a;
+//
+//    for (int q = 0; q < Q; ++q) {
+//        for (int i = 0; i < k; ++i)
+//            ProcSum += localArray[i];
+//    }
+//    delete[] localArray;
+//#ifdef Point_Point
+//    if (ProcRank == 0) {
+//        printf("\nType = Point-Point");
+//        TotalSum = ProcSum;
+//        for (int i = 1; i < ProcNum; ++i) {
+//            MPI_Recv(&ProcSum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &Status);
+//            TotalSum = TotalSum + ProcSum;
+//        }
+//        TotalSum /= Q;
+//    }
+//    else
+//        MPI_Send(&ProcSum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+//#endif
+//#ifdef Collect
+//    if (ProcRank == 0) printf("\nType = Collect");
+//    MPI_Reduce(&ProcSum, &TotalSum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+//    TotalSum /= Q;
+//#endif
+//    MPI_Barrier(MPI_COMM_WORLD);
+//    end_time = MPI_Wtime();
+//    end_time = end_time - st_time;
+//
+//    if (ProcRank == 0) {
+//        printf("\nSum = %10.2f", TotalSum);
+//        printf("\nTime = %f ", end_time);
+//    }
+//    MPI_Finalize();
+//}
